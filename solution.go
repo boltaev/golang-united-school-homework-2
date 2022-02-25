9c0b751121a98baa0bffd8cbb3ad97cafaad606b
boltaev <lazizbold@gmail.com>
2022-02-25 20:49:58 +0500

    send third attempt

--- solution.go
+++ solution.go
@@ -10,12 +10,13 @@ const (
 	SidesCircle   = 0
 )
 
+// CalcSquare formulas changed a bit(except case 3) cause of using all constants
 func CalcSquare(sideLen float64, sidesNum sides) (square float64) {
 	switch sidesNum {
 	case 0:
 		square = math.Pow(sideLen, 2)*math.Pi + SidesCircle
 	case 4:
-		square = sideLen * SidesSquare
+		square = sideLen * SidesSquare / 2
 	case 3:
 		square = math.Pow(sideLen, 2) / 4 * math.Sqrt(SidesTriangle)
 	}
